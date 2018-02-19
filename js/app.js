69719ba1e087355664bb976fbea98b3f38d1d90b
Danielalab <olabolacrayola@hotmail.com>
2018-02-19 10:30:13 -0500

    cambia colot

--- js/app.js
+++ js/app.js
@@ -14,29 +14,6 @@ window.onload = () => {
   let divContainerBgImg = $('#work .container-bg-img');
   let spanNameProyect;
 
-  // funcionalidad para mostrar cada proyecto
-
-  let getDataProyect = (id) => {
-    id = parseInt(id);
-    let dataProyect = proyects[id - 1];
-    console.log(dataProyect);
-    nameProyect.text(dataProyect.name);
-    description.text(dataProyect.description);
-    btnSite.attr('href', 'dataProyect.url');
-    btnGithub.attr('href', 'dataProyect.github');
-    imgModal.attr('src', `assets/images/${id}.png`);
-  }; 
-
-  let showProyect = (event) => {
-    event.preventDefault();
-    let id = event.target.id;
-    getDataProyect(id); 
-  };
-
-  [...proyectsContainers].forEach(element => {
-    $(element).on('click', showProyect);
-  });
-
   // funcionalidad para efecto de presentación de los proyectos en la sección portafolio 
   function showText () {
     $(this).css('color', '#ffffff');;
